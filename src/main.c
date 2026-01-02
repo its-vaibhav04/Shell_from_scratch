f7e2029305a1fc1698cbbc173e94dbb1e69a79eb
its-vaibhav04 <tyagi.vaibhav4814@gmail.com>
2026-01-02 12:03:30 +0530

    Exit Clause

--- src/main.c
+++ src/main.c
@@ -5,20 +5,23 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL); //remove the buffer of stdout i.e printing directly & not storing
+
+  //REPL - Read Evaluate Print Loop
+  char input[100]; //declaring a char array to store input command of user
   while(1){
 
     printf("$ ");
 
-    char input[100]; //declaring a char array to store input command of user
-
     //fgets(where to store the input, maximum capacity of input, from where the input is taken)
     fgets(input,100,stdin);
+    //Can do scanf() as well
 
     // TODO: Uncomment the code below to pass the first stage
 
     //strcspn(x,y) -> Read string x until any character from y matches (return the index of match)
     //command[index] = '\0' -> Replacing next line char with null terminator
     input[strcspn(input,"\n")] = '\0';
+    if(strcmp(input,"exit")==0) break;
     printf("%s: command not found\n",input);
   }
 
