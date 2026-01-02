7c95c57a5ca298b88bb0d535fdf68329b768cbda
its-vaibhav04 <tyagi.vaibhav4814@gmail.com>
2026-01-02 11:54:23 +0530

    REPL

--- src/main.c
+++ src/main.c
@@ -5,19 +5,22 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL); //remove the buffer of stdout i.e printing directly & not storing
-  printf("$ ");
+  while(1){
 
-  char input[100]; //declaring a char array to store input command of user
+    printf("$ ");
 
-  //fgets(where to store the input, maximum capacity of input, from where the input is taken)
-  fgets(input,100,stdin);
+    char input[100]; //declaring a char array to store input command of user
 
-  // TODO: Uncomment the code below to pass the first stage
+    //fgets(where to store the input, maximum capacity of input, from where the input is taken)
+    fgets(input,100,stdin);
 
-  //strcspn(x,y) -> Read string x until any character from y matches (return the index of match)
-  //command[index] = '\0' -> Replacing next line char with null terminator
-  input[strcspn(input,"\n")] = '\0';
-  printf("%s: command not found",input);
+    // TODO: Uncomment the code below to pass the first stage
+
+    //strcspn(x,y) -> Read string x until any character from y matches (return the index of match)
+    //command[index] = '\0' -> Replacing next line char with null terminator
+    input[strcspn(input,"\n")] = '\0';
+    printf("%s: command not found",input);
+  }
 
   return 0;
 }
