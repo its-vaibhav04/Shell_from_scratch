d8283803417704277e4c6e235b4996609e3ded8e
its-vaibhav04 <tyagi.vaibhav4814@gmail.com>
2026-01-02 15:23:02 +0530

    Path command

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -36,10 +40,49 @@ int main(int argc, char *argv[]) {
               }
           }
 
-          if (found)
+          if (found){
               printf("%s is a shell builtin\n",input+5);
-          else
-              printf("%s: not found\n",input+5);
+          }else{
+              bool executable = false;
+              char path = getenv("PATH");
+              char target = input+5;
+
+              if(path){
+                char path_copy = strdup(path);
+                char token = strtok(path_copy,";");
+
+                while(token){
+                  DIR *dir = opendir(token);
+                  if(dir){
+                    struct dirent *entry;
+                    while((entry = readdir(dir))!=NULL){
+                      if(strcmp(entry->d_name,target)==0){
+                        char full_path[1024];
+                        snprintf(full_path,sizeof(full_path),"%s/%s",token,target);
+                        if (access(full_path, X_OK) == 0) {
+                          struct stat path_stat;
+                          stat(full_path, &path_stat);
+
+                          if (S_ISREG(path_stat.st_mode)) {
+                            printf("%s is %s\n", entry->d_name, full_path);
+                            executable = true;
+                            break;
+                          }
+                        }
+                      }
+                    }
+                    closedir(dir);
+                  }
+                  if (executable)
+                    break;
+                  token = strtok(NULL, ":");
+                }
+                free(path_copy);
+              }
+              if (!executable) {
+                printf("%s: not found\n",target);
+              }
+          }
     }else{
       printf("%s: command not found\n",input);
     }
