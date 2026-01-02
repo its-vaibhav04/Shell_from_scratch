d8b6a7f996ac7ced634e349b4c4b9d4f1f9b034e
its-vaibhav04 <tyagi.vaibhav4814@gmail.com>
2026-01-02 12:34:59 +0530

    typr command

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
 
   //REPL - Read Evaluate Print Loop
   char input[100]; //declaring a char array to store input command of user
+  char builtin[3][10]={"echo","exit","type"};
   while(1){
 
     printf("$ ");
@@ -25,6 +26,20 @@ int main(int argc, char *argv[]) {
 
     if(strncmp(input, "echo ", 5) == 0){
       printf("%s\n",input+5);
+    }else if(strncmp(input, "type ", 5) == 0){
+          int found = 0;
+
+          for (int i = 0; i < 4; i++) {
+              if (strcmp(builtin[i], input) == 0) {
+                  found = 1;
+                  break;
+              }
+          }
+
+          if (found)
+              printf("%s is a shell builtin\n",input+5);
+          else
+              printf("%s: not found\n",input+5);
     }else{
       printf("%s: command not found\n",input);
     }
